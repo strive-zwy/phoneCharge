58e82d921ef1b580cf5c17bd46fe1b1cf2f0db7f
strive-zwy <1228304424@qq.com>
2020-12-29 08:43:24 +0800

    话费充值系统-测试01

---
+++ src/com/zwy/Test.java
@@ -0,0 +1,15 @@
+package com.zwy;
+
+/**
+ * @Author ：zwy
+ * @Date ：2020/12/29 8:42
+ * @Version ：1.0
+ * @Description ：TODO
+ **/
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(00000);
+    }
+}
