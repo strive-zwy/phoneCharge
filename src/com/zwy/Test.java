07195a8c1fa8df62cb0b17eb34a32e67a546e3bc
ZFJ-111 <337582797@qq.com>
2020-12-29 10:35:28 +0800

    新建测试-02

--- src/com/zwy/Test.java
+++ src/com/zwy/Test.java
@@ -12,6 +12,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(00000);
         System.out.println(00000);
-        System.out.println(00000);
+        System.out.println(11111);
     }
 }
